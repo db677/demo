ee12edfb7738e917b182126f38ff13b6a49830e7
Rex.Tan <admin@hiya3d.com>
2021-12-13 16:48:48 +0800

    fix

--- picture-fix/src/main/java/com/hiya3d/picturefix/jvm/JvmUtil.java
+++ picture-fix/src/main/java/com/hiya3d/picturefix/jvm/JvmUtil.java
@@ -119,36 +119,10 @@ public class JvmUtil {
 
 	public Map<String, Object> cpu() {
 		Map<String, Object> result = new HashMap<>();
-		SystemInfo systemInfo = new SystemInfo();
-		result.put("程序启动时间", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-				.format(new Date(ManagementFactory.getRuntimeMXBean().getStartTime())));
-		result.put("程序更新时间", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-				.format(new Date(ManagementFactory.getRuntimeMXBean().getUptime())));
-		CentralProcessor processor = systemInfo.getHardware().getProcessor();
-		long[] prevTicks = processor.getSystemCpuLoadTicks();
-		long[] ticks = processor.getSystemCpuLoadTicks();
-		long nice = ticks[CentralProcessor.TickType.NICE.getIndex()]
-				- prevTicks[CentralProcessor.TickType.NICE.getIndex()];
-		long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()]
-				- prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
-		long softirq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()]
-				- prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
-		long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()]
-				- prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
-		long cSys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()]
-				- prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
-		long user = ticks[CentralProcessor.TickType.USER.getIndex()]
-				- prevTicks[CentralProcessor.TickType.USER.getIndex()];
-		long iowait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()]
-				- prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
-		long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()]
-				- prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
-		long totalCpu = user + nice + cSys + idle + iowait + irq + softirq + steal;
-		result.put("cpu核数", processor.getLogicalProcessorCount());
-		result.put("cpu系统使用率", new DecimalFormat("#.##%").format(cSys * 1.0 / totalCpu));
-		result.put("cpu用户使用率", new DecimalFormat("#.##%").format(user * 1.0 / totalCpu));
-		result.put("cpu当前等待率", new DecimalFormat("#.##%").format(iowait * 1.0 / totalCpu));
-		result.put("cpu当前空闲率", new DecimalFormat("#.##%").format(idle * 1.0 / totalCpu));
+		OperatingSystemMXBean systemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+		result.put("cpu系统使用率", new DecimalFormat("#.##%").format(systemMXBean.getSystemCpuLoad()));
+		result.put("cpuJVM使用率", new DecimalFormat("#.##%").format(systemMXBean.getProcessCpuLoad()));
+		result.put("cpu核心数", systemMXBean.getAvailableProcessors());
 		return result;
 	}
 
